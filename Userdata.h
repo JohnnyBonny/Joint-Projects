d5ad9c8fc73980a5622bccd5f7b2af4127ec8d70
JohnnyBonny <jonathanmillerschool@gmail.com>
2021-12-28 21:50:45 -0800

    Created a BigInt file and data file that contains the user's information. I also added some test functions in the main

---
+++ Userdata.h
@@ -0,0 +1,70 @@
+
+#ifndef JOINT_PROJECTS_USERDATA_H
+#define JOINT_PROJECTS_USERDATA_H
+
+#include <string.h>
+#include <iostream>
+#include "BigInt.h"
+
+using namespace std;
+ 
+class Userdata{
+    private:
+    BigInt userClicks; // number of clicks for the user
+    double passiveMultiplier; // number of passive clicks per second
+    double clickMultiplier; //muliplier of clicks the users recieves when they click
+
+    public:
+        Userdata(){
+            //number of userClicks is determined in the private variable
+            passiveMultiplier = 1.0;
+            clickMultiplier = 1.0;
+        };
+
+        // returns the number of clicks the users has 
+        BigInt getUserClicks(){
+            return userClicks;
+        };
+
+        //returns the passive multiplier of user
+        double getPassiveMultiplier(){
+            return passiveMultiplier;
+        };
+
+        //returns the multiplier when the user manually clicks
+        double getClickMulitplier(){
+            return clickMultiplier;
+        };
+
+        //sets the user's clicks
+        void setUserClicks(BigInt x){
+            userClicks= x;
+        };
+
+        //sets the user's passive multiplier
+        void setPassiveMultiplier(double x){
+            passiveMultiplier = x;
+        };
+
+        //sets the click multiplier
+        void setClickMulitplier(double x){
+            clickMultiplier = x;
+        };
+        
+        //adds to the user's clicks
+        void addtoUserClicks(BigInt x){
+            userClicks= (userClicks + x);
+        };
+
+        //adds to the Passive Multiplier
+        void addtoPassiveMulitplier(double x){
+            passiveMultiplier += x;
+        };
+
+        //adds to the click multiplier
+        void addtoClickMulitplier(double x){
+            clickMultiplier += x;
+        };
+        
+};
+#endif //JOINT_PROJECTS_USERDATA_H
--- clickergame.cpp
+++ clickergame.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include "Userdata.h"
 #include <conio.h>
 #include <windows.h>
+#include "BigInt.h"
+#include <string.h>
 
 using namespace std;
 
-int main(){
+void executeClicks(){
     while (1){
         if (GetAsyncKeyState(0x01)){
             cout << "Left Mkey";
@@ -14,6 +17,29 @@ int main(){
     }
 
     getch();
-    cout << "Hi" << endl;
+}
+
+int main(){
+    Userdata myscore = Userdata();
+    cout << "My clicks:" << myscore.getUserClicks() << endl;
+    printf("My clicksMultiplier:%.02f\n" , myscore.getClickMulitplier());
+    printf("My passiveMultiplier:%.02f\n" , myscore.getPassiveMultiplier());
+
+    myscore.setClickMulitplier(1.25);
+    myscore.setPassiveMultiplier(1.25);
+    myscore.setUserClicks(to_string(12456432133));
+    cout << "\nMy clicks:" << myscore.getUserClicks() << endl;
+    printf("My clicksMultiplier:%.02f\n" , myscore.getClickMulitplier());
+    printf("My passiveMultiplier:%.02f\n" , myscore.getPassiveMultiplier());
+
+    myscore.addtoClickMulitplier(.75);
+    myscore.addtoPassiveMulitplier(.75);
+
+    BigInt x("123472389473473897491");
+    myscore.addtoUserClicks(x);
+    cout << "\nMy clicks:" << myscore.getUserClicks() << endl;
+    printf("My clicksMultiplier:%.02f\n" , myscore.getClickMulitplier());
+    printf("My passiveMultiplier:%.02f\n" , myscore.getPassiveMultiplier());
+
     return 0;                                                                                               
 }
